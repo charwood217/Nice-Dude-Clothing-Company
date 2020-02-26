3d5059e0969b93d056b504f7004b60cd4cac4709
Christian Harwood <charwood217@west-mec.org>
2020-02-25 18:21:08 -0700

    Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*  Author: Christian Harwood
+    Date: 2/26/20
+    Checked By: Daniel Garcia
+    Project Manager: Christian Harwood
+    Web Developer: Christian Harwood
+    Web Designer: Brandon Santizo
+    Content Designer: Domonic Torres
+    Programmer: Daniel Garcia
+*/
+
+function styleSwap(sheet){
+    document.getElementById('theme').setAttribute('href', sheet);
+}
